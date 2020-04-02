ce22b10d15120a81a881e416871490b6849ef8f2
song <songsm5504@gmail.com>
2020-04-02 15:30:43 +0900

    commit

---
+++ src/components/Words.js
@@ -0,0 +1,197 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { widthStyles, withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextFiled from "@material-ui/core/TextField";
+
+const styles = theme => ({
+  fab: {
+    position: "fixed",
+    bottom: "20px",
+    right: "20px"
+  }
+});
+
+const databaseURL = "https://word-cloud-2196e.firebaseio.com/";
+
+class Words extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      words: {},
+      dialog: false,
+      word: "",
+      weight: ""
+    };
+  }
+
+  _get() {
+    fetch(`${databaseURL}/words.json`)
+      .then(res => {
+        if (res.status != 200) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then(words => this.setState({ words: words }));
+  }
+
+  _post(word) {
+    return fetch("${datebaseURL}/words.json", {
+      method: "POST",
+      body: JSON.stringify(word)
+    })
+      .then(res => {
+        if (res.status != 200) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then(data => {
+        let nextState = this.state.words;
+        nextState[databaseURL.name] = words;
+        this.setState({ words: nextState });
+      });
+  }
+
+  _delete(id) {
+    return fetch(`${databaseURL}/words/${id}.json`, {
+      method: "DELETE"
+    })
+      .then(res => {
+        if (res.status != 200) {
+          throw new Error(res.statusText);
+        }
+        return res.json();
+      })
+      .then(() => {
+        let nextState = this.state.words;
+        delete nextState[id];
+        this.setState({ words: nextState });
+      });
+  }
+
+  componentDidMount() {
+    this._get();
+  }
+
+  handelDialogToggle = () =>
+    this.setState({
+      dialog: !this.state.dialog
+    });
+
+  handleValueChange = e => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  };
+
+  handleSubmit = () => {
+    const word = {
+      word: this.state.word,
+      weight: this.state.weight
+    };
+    this.handelDialogToggle();
+    if (!word.word && !word.weight) {
+      return;
+    }
+    this._post(word);
+  };
+
+  handelDelet = id => {
+    this._delete(id);
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        {Object.keys(this.state.words).map(id => {
+          const word = this.state.words[id];
+          return (
+            <div key={id}>
+              <Card>
+                <CardContent>
+                  <Typography color="textSecondary" gutterBottom>
+                    가중치 : {word.weight}
+                  </Typography>
+                  <Grid container>
+                    <Grid item xs={6}>
+                      <Typography variant="h5" component="h2">
+                        {word.word}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => this.handelDelet(id)}
+                      >
+                        삭제
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Card>
+              <br />
+            </div>
+          );
+        })}
+        <Fab
+          color="primary"
+          className={classes.fab}
+          onClick={this.handelDialogToggle}
+        >
+          <AddIcon />
+        </Fab>
+        <Dialog open={this.state.dialog} onClose={this.handelDialogToggle}>
+          <DialogTitle>단어추가</DialogTitle>
+          <DialogContent>
+            <TextFiled
+              label="단어"
+              type="text"
+              name="word"
+              value={this.state.word}
+              onChange={this.handleValueChange}
+            />
+            <br />
+            <TextFiled
+              label="가중치"
+              type="number"
+              name="weight"
+              value={this.state.weight}
+              onChange={this.handleValueChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleSubmit}
+            >
+              추가
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={this.handelDialogToggle}
+            >
+              닫기
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Words);
